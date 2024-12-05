b3b54107a7d4c5c51595f6da74ec56a820d75892
clemilson chaves <cle_milson_silva@hotmail.com>
2024-12-05 05:05:03 -0300

    remove comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import styles from './Comment.module.css'; 
 import { Avatar } from './Avatar';
-export function Comment() {   
+
+export function Comment({content, onDeleteComment}) {   
+  const [ likeCount, setLikeCount ] = useState(0);
+ 
+
+  function handleDeleteComment(){
+    onDeleteComment(content)
+  } 
+  
+  function handleLikeComment(){
+    setLikeCount(likeCount + 1);
+  }
+  
   return (
     <div className={styles.comment}>
       <Avatar  hasBorder={false} src="https://github.com/clemilsonchaves.png" alt="Foto do Clemilson Chaves" />
@@ -16,17 +29,17 @@ export function Comment() {
               <time title="07 de junho às 04:19h" dateTime="2024-06-07 04:18:30">Cerca de uma 1h atrás</time>  
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24}/>
             </button>
           </header>
-          <p>Muito bom Devon, Parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-         <button>
+         <button onClick={handleLikeComment}>
           <ThumbsUp size={24} />
-          Aplaudir <span>20</span>      
+          Aplaudir <span>{likeCount}</span>      
          </button>
         </footer>
       </div>
